19db3d9da6b812cefa34164f7c945ca7fdc31834
taurus366 <taurus.ali47@gmail.com>
2025-11-29 19:57:12 +0200

    init

---
+++ src/main/java/com/aiolds/cl_util/ClUtilApplication.java
@@ -0,0 +1,13 @@
+package com.aiolds.cl_util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClUtilApplication.class, args);
+    }
+
+}
